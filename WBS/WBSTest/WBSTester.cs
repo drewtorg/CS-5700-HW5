24aac4a984dd9e066bbceeab37d40fea0ee6afb9
Drew Torgeson <drew.torgeson@gmail.com>
2015-11-18 15:41:29 -0700

    Initial commit

---
+++ WBS/WBSTest/WBSTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WBS;
+using NUnit.Framework;
+
+namespace WBSTest
+{
+    [TestFixture]
+    public class WBSTester
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}

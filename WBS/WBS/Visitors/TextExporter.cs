ed8fca1d76aca94b75ae02884036b002bd771bd1
Drew Torgeson <drew.torgeson@gmail.com>
2015-11-20 16:32:58 -0700

    Implemented TextExporter as Visitor

---
+++ WBS/WBS/Visitors/TextExporter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WBS.Tasks;
+
+namespace WBS.Visitors
+{
+    public class TextExporter : Visitor
+    {
+        private StreamWriter writer;
+        string tabs;
+
+        public void WriteToFile(string filename, Task task)
+        {
+            tabs = "";
+            using (writer = new StreamWriter(File.OpenWrite(filename)))
+            {
+                writer.WriteLine("Work Breakdown Structure Outline\n");
+                Visit((dynamic) task);
+            }
+        }
+
+        protected override void Visit(ParallelParentTask task)
+        {
+            writer.WriteLine($"{tabs}Parallel Task - {task.Label}");
+            tabs += "\t";
+            base.Visit(task);
+            tabs = tabs.Remove(0,1);
+        }
+
+        protected override void Visit(SequentialParentTask task)
+        {
+            writer.WriteLine($"{tabs}Sequential Task - {task.Label}");
+            tabs += "\t";
+            base.Visit(task);
+            tabs = tabs.Remove(0, 1);
+        }
+
+        protected override void Visit(LeafTask task)
+        {
+            writer.WriteLine($"{tabs}Task - {task.Label}");
+        }
+    }
+}
--- WBS/WBS/WorkBreakdownStructure.cs
+++ WBS/WBS/WorkBreakdownStructure.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
+using WBS.Tasks;
+using WBS.Visitors;
 
 namespace WBS
 {
     public class WorkBreakdownStructure
     {
         private Task root;
+        private Scheduler schedule;
 
         public WorkBreakdownStructure()
         {
             root = null;
+        }
+
+        public List<Task> GetAssignedTasks(Engineer e)
+        {
+            return schedule.TaskList[e];
+        }
+
+        public void Print(string filename)
+        {
+            new TextExporter().WriteToFile(filename, root);
         }
     }
 }

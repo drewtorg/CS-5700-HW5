4c6de2f276f33e2d2f4e53d8e61a73bad1f4dd50
Drew Torgeson <drew.torgeson@gmail.com>
2015-12-01 16:33:39 -0700

    Added xml implementations for task and wbs

---
+++ WBS/WBS/XML/TaskImportExporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+using WBS.Tasks;
+
+namespace WBS.XML
+{
+    public class TaskImportExporter : XMLImporterExporter<Task>
+    {
+        private static XmlSerializer serializer = new XmlSerializer(typeof(Task));
+
+        public void Export(Task wbs, string path)
+        {
+            using (var stream = File.OpenWrite(path))
+            {
+                serializer.Serialize(stream, wbs);
+            }
+        }
+
+        public Task Import(string path)
+        {
+            Task t = null;
+            using (var stream = File.OpenWrite(path))
+            {
+                t = serializer.Deserialize(stream) as Task;
+            }
+            return t;
+        }
+    }
+}
---
+++ WBS/WBS/XML/WBSImportExporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+using WBS.Tasks;
+
+namespace WBS.XML
+{
+    public class WBSImportExporter : XMLImporterExporter<WorkBreakdownStructure>
+    {
+        private static XmlSerializer serializer = new XmlSerializer(typeof(WorkBreakdownStructure));
+
+        public void Export(WorkBreakdownStructure wbs, string path)
+        {
+            using (var stream = File.OpenWrite(path))
+            {
+                serializer.Serialize(stream, wbs);
+            }
+        }
+
+        public WorkBreakdownStructure Import(string path)
+        {
+            WorkBreakdownStructure wbs = null;
+            using (var stream = File.OpenWrite(path))
+            {
+                wbs = serializer.Deserialize(stream) as WorkBreakdownStructure;
+            }
+            return wbs;
+        }
+    }
+}
